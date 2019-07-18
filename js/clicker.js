f30cff0792f7891bb8524986356d16c8d80afaa0
Toast-dk <daviddkou@gmail.com>
2019-07-18 10:09:31 -0700

    improved clicker upgrdes

--- js/clicker.js
+++ js/clicker.js
@@ -1,19 +1,24 @@
 //javascript file
 //cash
 var numOfClicks = 0;
+//clicking Power
+var clickPower = 1;
+//power upgrade cost
+var pwrCost = 50;
+//upgrade cost
+var upPrice = 50;
+
 function clickerUpdate() {
-  numOfClicks += 1;
+  numOfClicks += clickPower;
   document.getElementById("clicks").innerHTML = numOfClicks + " eggs";
 }
-function clickerUpdate2() {
-  numOfClicks += 2;
-  document.getElementById("clicks").innerHTML = numOfClicks + " eggs";
-}
-function clickerUpdate2Launch() {
-  if (numOfClicks >= 50) {
-    document.getElementById( "clickA" ).setAttribute( "onclick", "javascript: clickerUpdate2();" );
-    numOfClicks -= 50;
+function clickerPowerLaunch() {
+  if (numOfClicks >= upPrice) {
+    clickPower += 1;
+    numOfClicks -= upPrice;
+    upPrice += 10;
     document.getElementById("clicks").innerHTML = numOfClicks + " eggs";
+    document.getElementById('upgradePrice').innerHTML = "+1 Click Power (" + upPrice + " eggs)"
   } else {
     alert("Not Enough Money")
   }
